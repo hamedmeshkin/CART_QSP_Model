3c08e0c62bab3bd6f5dabbaebae1d8633c1c9b19
Hamed Meshkin <78892599+hamedmeshkin@users.noreply.github.com>
2025-11-09 23:33:50 -0500

    Delete simulationfunctions/getNegativityError.R

--- simulationfunctions/getNegativityError.R
+++
@@ -1,7 +0,0 @@
-getNegativityError<-function(list_of_out, namesyout){
-	shorter_namesyout<-namesyout[namesyout != "Chemoreflex drive (l/min)"]
-	negativeerror<-sapply(list_of_out, function(x) sum(pmin(x[,shorter_namesyout],0)^2))
-	negativeerror<-sum(negativeerror)
-	negativeerror
-	
-}
